a5f04d4b86ea27d07d513b7bb81e8a0b94bafd55
GarrettThing <garrett.clark0103@gmail.com>
2024-09-09 09:26:37 -0500

    buggy check win

--- board.py
+++ board.py
@@ -15,26 +15,62 @@ class Board:
             print(line)
             line = ""
 
-    def move_peg(self, starting_pos: Tuple[int, int], ending_pos: Tuple[int, int]):
-        pass
-
     def is_valid_move(self, starting_pos: Tuple[int, int], ending_pos: Tuple[int, int]) -> bool:
-        if self.layout[ending_pos[0]][ending_pos[1]] == "o":
+        if (ending_pos[0] < 0 or ending_pos[1] < 0 or
+                starting_pos[0] < 0 or starting_pos[1] < 0 or
+                starting_pos[0] > 4 or ending_pos[0] > 4 or
+                starting_pos[0] < starting_pos[1] or ending_pos[0] < ending_pos[1]):
+            return False
+        if self.layout[ending_pos[0]][ending_pos[1]] == "o" and self.layout[starting_pos[0]][starting_pos[1]] == "@":
             change_y = ending_pos[0] - starting_pos[0]
             change_x = ending_pos[1] - starting_pos[1]
-            if change_x == -2 or 0 or 2 and change_y == -2 or 0 or 2:
+            if (change_x == -2 or change_x == 0 or change_x == 2) and (change_y == -2 or change_y == 0 or change_y == 2):
                 if change_x == 0 or change_y == 0:
                     if change_x == 0 and change_y == 0:
                         return False
+                    elif self.layout[starting_pos[0] + change_y//2][starting_pos[1] + change_x//2] == "@":
+                        return True
                 elif change_y/change_x != -1:
                     if self.layout[starting_pos[0] + change_y//2][starting_pos[1] + change_x//2] == "@":
                         return True
-        else:
-            return False
+        return False
 
-    def is_solved(self) -> bool:
-        pass
+
+    def move_peg(self, starting_pos: Tuple[int, int], ending_pos: Tuple[int, int]):
+        if self.is_valid_move(starting_pos, ending_pos):
+            self.layout[ending_pos[0]][ending_pos[1]] = "@"
+            self.layout[starting_pos[0]][starting_pos[1]] = "o"
+            middle_y = (starting_pos[0] + ending_pos[0])//2
+            middle_x = (starting_pos[1] + ending_pos[1])//2
+            self.layout[middle_y][middle_x] = "o"
+
+    def is_game_over(self) -> str:
+        peg_pos = 0
+        for pos_y in range(5):
+            for pos_x in range(pos_y + 1):
+                if self.layout[pos_y][pos_x] == "@":
+                    peg_pos += 1
+                    for end_pos_y in range(-2,4,2):
+                        for end_pos_x in range(-2,4,2):
+                            if self.is_valid_move((pos_y,pos_x),(end_pos_y,end_pos_x)):
+                                return "continue"
+
+        if peg_pos == 1:
+            return "win"
+        else:
+            return "lose"
 
 
 board = Board()
 board.display()
+board.move_peg((2,0),(0,0))
+board.move_peg((2,2),(2,0))
+board.move_peg((3,0),(1,0))
+board.move_peg((0,0),(2,2))
+board.move_peg((3,3),(1,1))
+board.move_peg((4,1),(2,1))
+board.move_peg((1,1),(3,1))
+board.move_peg((4,3),(4,1))
+board.move_peg((4,1),(2,1))
+board.display()
+print(board.is_game_over())
